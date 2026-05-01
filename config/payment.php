2e57256bb2160da9efe6997b0a1b20b5747c1544
Simonchan-openclaw <simon@example.com>
2026-05-02 04:57:52 +0800

    update: 更新易支付商户配置

--- config/payment.php
+++ config/payment.php
@@ -5,8 +5,8 @@
 return [
     // 易支付配置
     'yipay' => [
-        'pid' => '1001',  // 商户ID
-        'key' => '89unJUB8HZ54Hj7x4nUj56HN4nUzUJ8i',  // 商户密钥
-        'api_url' => 'https://icu.zd16688.com',
+        'pid' => '1006',  // 商户ID
+        'key' => 'sMxhHZTTwHwssbWBLLbSGMxm9T2x2g94',  // 商户密钥
+        'api_url' => 'https://icu.zd16688.com/',
     ],
 ];
